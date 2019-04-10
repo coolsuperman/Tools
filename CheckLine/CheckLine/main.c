82ee6a08002645f6b273520f0c49120174f85e87
Coolsuperman <846069147@qq.com>
2019-04-11 00:34:08 +0800

    "检查路径下所有目标文件行数V1.01-参数稳定性优化，增加了自由指定路径/文件类型选项"

--- CheckLine/CheckLine/main.c
+++ CheckLine/CheckLine/main.c
@@ -3,16 +3,31 @@
 #include<string.h>
 #include<io.h>
 
-void _GetName (char*path);
-
-char* root_path = "D:\\Code\\";
-char* search = ".h";
-char* check = "*";
-char* suffix = "\\";//后缀；
-int count = 0;
 typedef struct _finddata_t Data;
 
-int CountLine (char*PATH,const char* file_name) {
+void _GetName (const char*path,const char* search);
+typedef struct NeedToUse {//封装隔绝，避免误用；
+	char* check  ;//检查通配符
+	char* suffix  ;//后缀；
+	int count  ;//行数记录
+	int count_file;//文件个数
+}NeedToUse;
+
+static NeedToUse NTU = { "*" , "\\" , 0 ,0};
+
+static int IsSameSuffix (const char*FileInfo_name , const char*search) {//倒着判断后缀是否相等；
+	int len_name = strlen (FileInfo_name);
+	int len_search = strlen (search);
+	for ( int i = 0; i < len_search; i++ ) {
+		if ( FileInfo_name [len_name - i] != search [len_search - i] ) {
+			return 0;
+		}
+	}
+	return 1;
+}
+
+int CountLine (const char*PATH, const char* file_name) {
+	NTU.count_file++;
 	int _count = 0;
 	int ch = 0;
 	char file [260] = { 0 };
@@ -33,68 +48,62 @@ int CountLine (char*PATH,const char* file_name) {
 	return _count;
 }
 
-void filecheck (char* path , Data*FileInfo) {
+void Filecheck (const char* path , Data*FileInfo,const char*search) {
 	if ( FileInfo->attrib == _A_SUBDIR ) {//子文件
-		char grandchild_path [260] = { 0 };
 		if ( (strcmp (FileInfo->name , ".")) && (strcmp (FileInfo->name , "..")) ) {
+			char grandchild_path [260] = { 0 };
 			strcat (strcpy (grandchild_path , path) , FileInfo->name);
-			strcat (grandchild_path , suffix);
-			_GetName (grandchild_path);
+			strcat (grandchild_path , NTU.suffix);
+			_GetName (grandchild_path,search);
 		}
 	}
 	else {//普通文件
-		if (IsSame(FileInfo->name,search)) {
+		if ( IsSameSuffix (FileInfo->name,search)) {
 			int add = CountLine (path,FileInfo->name);
 			printf ("%s:%d lines---PATH:%s\n" , FileInfo->name , add,path);
-			count += add;
+			NTU.count += add;
 		}
 	}
 }
 
-static int IsSame (const char*FileInfo_name , const char*search) {
-	int len_name = strlen (FileInfo_name);
-	int len_search = strlen (search);
-	for ( int i = 0; i < len_search; i++ ) {
-		if ( FileInfo_name [len_name - i] != search [len_search - i] ) {
-			return 0;
-		}
-	}
-	return 1;
-}
-
-void _GetName (char*path) {
+void _GetName (const char*path,const char* search) {
 	long handle;
 	Data  FileInfo;
 	char new_path [260] = { 0 };
-	handle = _findfirst (strcat (strcpy (new_path , path) , check) , &FileInfo);
+	handle = _findfirst (strcat (strcpy (new_path , path) , NTU.check) , &FileInfo);
 	if ( handle == -1 ) {
 		printf ("No any files exist in File:%s\n" , path);
 		return;
 	}
-	else if ( FileInfo.attrib == _A_SUBDIR ) {//子文件
+	else if ( FileInfo.attrib == _A_SUBDIR ) {//子文件夹
 		if ( (!strcmp (FileInfo.name , ".")) && (!strcmp (FileInfo.name , "..")) ) {
 			char child_path [260] = { 0 };
 			strcat (strcpy (child_path , path) , FileInfo.name);
-			strcat (child_path , suffix);
-			_GetName (child_path);
+			strcat (child_path , NTU.suffix);
+			_GetName (child_path,search);
 		}
 	}
 	else {//普通文件
-		if ( IsSame (FileInfo.name , search) ) {
-			count += CountLine (path,FileInfo.name);
-			printf ("%s:%d lines---PATH:%s\n" , FileInfo.name , count,path);
+		if ( IsSameSuffix (FileInfo.name , search) ) {//找与目标后缀相同的文件；
+			NTU.count += CountLine (path,FileInfo.name);
+			printf ("%s:%d lines---PATH:%s\n" , FileInfo.name , NTU.count,path);
 		}
 	}
 	while ( !_findnext (handle , &FileInfo) ) {
-		filecheck (path , &FileInfo);
+		Filecheck (path , &FileInfo,search);
 	}
 	_findclose (handle);
 }
 
-
 int main () {
-	_GetName (root_path);
-	printf ("总计书写了%d行代码，你已经超越了全国%%99.99的Coder！！！\n",count);
+	char search [260] = {0};
+	char root_path [260] = { 0 };
+	printf ("请输入你要查找的路径(C:\\\\example\\\\example\\\\):\n");
+	scanf ("%s" , &root_path);
+	printf ("请输入你要统计的文件后缀(.c):\n");
+	scanf ("%s" , &search);
+	_GetName (root_path,search);
+	printf ("你的共%d个%s文件里%d行代码，还不错，但继续加油！！！\n",NTU.count_file,search,NTU.count);
 	system ("pause");
 	return 0;
 }
